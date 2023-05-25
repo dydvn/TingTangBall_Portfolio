f6ce75e7bf24f577d87e3b9f9029903d1cb22576
김용원 <62327209+dydvn@users.noreply.github.com>
2023-05-25 19:43:35 +0900

    Update CamShake.cs

--- CamShake.cs
+++ CamShake.cs
@@ -1,13 +1,13 @@
 private IEnumerator Cam_Shake()
+{
+    isCam_Shake = true;
+    float fCount = 0;
+    while (fCount < fDuration)
     {
-        isCam_Shake = true;
-        float fCount = 0;
-        while (fCount < fDuration)
-        {
-            v3_ffffffffffff = Random.insideUnitSphere * fShake_Power;
-            cam_Main.transform.position = cam_Main.transform.position + v3_ffffffffffff;
-            fCount += 1 * Time.deltaTime;
-            yield return null;
-        }
-        isCam_Shake = false;
+        v3_ShakePos = Random.insideUnitSphere * fShake_Power;
+        cam_Main.transform.position = cam_Main.transform.position + v3_ShakePos;
+        fCount += 1 * Time.deltaTime;
+        yield return null;
     }
+    isCam_Shake = false;
+}
